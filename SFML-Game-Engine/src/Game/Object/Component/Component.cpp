0c3481c8945a6343aceaace885a1a5d83f008081
Poulpynateur <45163494+Poulpynateur@users.noreply.github.com>
2019-07-26 10:10:58 +0200

    Why VS ? why ???

--- SFML-Game-Engine/src/Game/Object/Component/Component.cpp
+++
@@ -1,7 +0,0 @@
-#include "PhysicComponent.h"
-#include "../Object.h"
-#include "../../World.h"
-
-void PhysicComponent::update(Object& object, World& world) {
-	std::cout << "Object component + " + world.saidSomething() << '\n';
-}
--- SFML-Game-Engine/src/Game/Object/Component/InputsComponent.cpp
+++
@@ -1,7 +0,0 @@
-#include "PlayerInputs.h"
-#include "../Object.h"
-#include "../../World.h"
-
-void PlayerInputs::update(Object& object, std::unordered_map<sf::Keyboard::Key, bool>& keys) {
-
-}
