b86fd18d12613b457e3aa851410ccbf02c000fc8
Poulpynateur <nicolas.degheselle@viacesi.fr>
2019-11-14 10:32:13 +0100

    Commit

--- SFML-Game-Engine/src/Game/Objects/Componants/PlayerInputs.cpp
+++ SFML-Game-Engine/src/Game/Objects/Componants/PlayerInputs.cpp
@@ -3,6 +3,9 @@
 #include "../Player.h"
 
 void PlayerInputs::update(Player* parent, World* world) {
+
+	// Some things
+
 	if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up)) {
 		parent->graphics->shape->setPosition(0, 0);
 	}
